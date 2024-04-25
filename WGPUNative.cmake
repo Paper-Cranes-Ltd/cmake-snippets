c0302b0e0797fa8b7902a5e667670ed0b52c4d14
Hristo Iliev <capitane71@gmail.com>
2024-04-25 09:20:06 +0300

    Include a WGPU Native module that loads the wgpu-native rust project, compiles it and creates a CMake target with the libraries.

---
+++ WGPUNative.cmake
@@ -0,0 +1,46 @@
+function(get_wgpu_native VERSION_TAG)
+    include(FindPackageHandleStandardArgs)
+    include(FetchContent)
+    
+    find_program(RUST_CARGO NAMES cargo REQUIRED)
+    
+    fetchcontent_declare(wgpu_native
+            GIT_REPOSITORY "https://github.com/gfx-rs/wgpu-native.git"
+            GIT_TAG "${VERSION_TAG}"
+            GIT_SUBMODULES_RECURSE YES
+            GIT_SHALLOW YES
+            GIT_REMOTE_UPDATE_STRATEGY CHECKOUT
+    )
+    
+    message(STATUS "Fetching wgpu-native")
+    fetchcontent_populate(wgpu_native)
+    
+    message(STATUS "Building wgpu-native")
+    set(ENV{RUSTFLAGS} "-Awarnings")
+    execute_process(COMMAND "${RUST_CARGO}" build --release --quiet --target-dir "${wgpu_native_BINARY_DIR}" --manifest-path "${wgpu_native_SOURCE_DIR}/Cargo.toml")
+    
+    add_library(wgpu_native STATIC IMPORTED)
+    set_target_properties(wgpu_native PROPERTIES IMPORTED_LOCATION "${wgpu_native_BINARY_DIR}/release/${CMAKE_STATIC_LIBRARY_PREFIX}wgpu_native${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    set_target_properties(wgpu_native PROPERTIES IMPORTED_CONFIGURATIONS "RELEASE")
+    
+    file(COPY
+            "${wgpu_native_SOURCE_DIR}/ffi/wgpu.h"
+            "${wgpu_native_SOURCE_DIR}/ffi/webgpu-headers/webgpu.h"
+            DESTINATION
+            "${wgpu_native_BINARY_DIR}/include/webgpu"
+    )
+    
+    target_include_directories(wgpu_native INTERFACE "${wgpu_native_BINARY_DIR}/include/")
+    
+    if(WIN32)
+        set(OS_LIBRARIES d3dcompiler ws2_32 userenv bcrypt ntdll opengl32)
+    elseif(UNIX AND NOT APPLE)
+        set(OS_LIBRARIES "-lm -ldl")
+    elseif(APPLE)
+        set(OS_LIBRARIES "-framework CoreFoundation -framework QuartzCore -framework Metal")
+    endif()
+    
+    target_link_libraries(wgpu_native INTERFACE ${OS_LIBRARIES})
+endfunction()
+
+
